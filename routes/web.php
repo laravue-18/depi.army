8cb89453d760be2c8c24c061cbfa541102fc987b
laravue-18 <yurijklimovv@gmail.com>
2022-04-08 22:56:55 +0800

    artisan command

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
@@ -41,4 +42,7 @@ Route::middleware('auth')->group(function(){
 });
 
 
-
+Route::get('cmd', function(){
+    $exitCode = Artisan::call('migrate');
+    dd($exitCode);
+});
