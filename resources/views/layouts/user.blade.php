9dd87832e5e15126d7edf7e3d9f0f4bfe3a76196
laravue-18 <77167796+laravue-18@users.noreply.github.com>
2022-05-11 20:48:39 +0800

    Logout Button

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -25,7 +25,7 @@
         <a href="/profile">Profile</a>
         <form action="logout" method="post">
             @csrf
-            <button type="submit" class="bg-transparent border-0 text-white">Logout</button>
+            <button type="submit" class="">Logout</button>
         </form>
     </div>
     <div class="admin-media">
