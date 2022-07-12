d7fb3001bcf4b3104d76470936f95725695b8db3
laravue-18 <77167796+laravue-18@users.noreply.github.com>
2022-07-12 22:46:44 +0800

    revert

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,7 +17,6 @@
     <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="/css/responsive.css">
     <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="/css/tailwind.min.css">
 
     @stack('css')
 </head>
